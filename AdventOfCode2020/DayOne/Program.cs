994b5c2b1bc384e64e11f3e29e77f8afaa7107f3
Dirk Schudde <dirk@degrosolutions.nl>
2020-12-09 11:19:28 +0100

    Day one
    Day one

---
+++ AdventOfCode2020/DayOne/Program.cs
@@ -0,0 +1,55 @@
+﻿using Shared;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOne
+{
+    internal class Program
+    {
+        private const string FilePath = @"C:\LocalDevelop\advent-of-code-2020\AdventOfCode2020\input\day-one.txt";
+
+        public IPuzzleInputReader InputReader { get; }
+
+        public Program(IPuzzleInputReader puzzleInputReader)
+        {
+            InputReader = puzzleInputReader;
+        }
+
+        private static async Task Main(string[] args)
+        {
+            IPuzzleInputReader puzzleInputReader = new PuzzleInputReader(FilePath);
+            var input = await puzzleInputReader.ReadPuzzleInputAsync();
+
+            var middle = input.Max() / 2 + 1;
+
+            var lowerNumbers = input.Where(results => results < middle);
+            var higherNumbers = input.Where(results => results >= middle);
+
+            int highResult = 0;
+            int lowResult = 0;
+
+            var highNumbers = higherNumbers as int[] ?? higherNumbers.ToArray();
+            foreach (var lowNumber in lowerNumbers)
+            {
+                if (highResult > 0 && lowResult > 0)
+                {
+                    break;
+                }
+
+                foreach (var highNumber in highNumbers)
+                {
+                    if (lowNumber + highNumber != 2020) continue;
+                    highResult = highNumber;
+                    lowResult = lowNumber;
+                    break;
+                }
+            }
+
+            int answer = lowResult * highResult;
+
+            Console.WriteLine($"Answer: {lowResult} * {highResult} = {answer}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2020/Shared/PuzzleInputReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public sealed class PuzzleInputReader : IPuzzleInputReader
+    {
+        private readonly string _filePath;
+
+        public PuzzleInputReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public async Task<int[]> ReadPuzzleInputAsync()
+        {
+            if (IsFilePathValid() == false)
+            {
+                Console.WriteLine("Provided filepath is invalid.");
+            }
+
+            try
+            {
+                using StreamReader reader = File.OpenText(_filePath);
+                string input = await reader.ReadToEndAsync();
+                string[] splittedResult = input.Split(Environment.NewLine);
+
+                return Array.ConvertAll(splittedResult, int.Parse);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Something went wrong while reading the input file: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong while processing the input file: {ex.Message}");
+                throw;
+            }
+        }
+
+        private bool IsFilePathValid()
+        {
+            try
+            {
+                Path.Combine(_filePath);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
