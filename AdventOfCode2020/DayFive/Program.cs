faef1d26e31be54160eb082a0371b3e99fb8e287
Dirk Schudde <dg.schudde@live.nl>
2020-12-14 12:51:55 +0100

    Day five part one
    Day five part one

---
+++ AdventOfCode2020/DayFive/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared;
+
+namespace DayFive
+{
+    internal static class Program
+    {
+        private const string FilePath = @"input/day-five.txt";
+
+        private static async Task Main()
+        {
+            IPuzzleInputReader puzzleInputReader = new PuzzleInputReader(FilePath);
+            string[] input = await puzzleInputReader.ReadPuzzleInputAsync<string[]>();
+            
+            var seatIds = new List<int>();
+
+            foreach (var entry in input)
+            {
+                string row = entry.Substring(0, 7);
+                
+                row = row.Replace("F", "0");
+                row = row.Replace("B", "1");
+                
+                string column = entry.Substring(7, 3);
+
+                column = column.Replace("L", "0");
+                column = column.Replace("R", "1");
+                
+                // Convert binary value to int
+                var columnValue = Convert.ToInt32(column, 2);
+                var rowValue = Convert.ToInt32(row, 2);
+                var seatId = (rowValue * 8) + columnValue;
+                seatIds.Add(seatId);
+            }
+
+            seatIds.Sort();
+            var maxSeatId = seatIds.Max(x => x);
+            Console.WriteLine($"Highest seat ID on a boarding pass is: {maxSeatId}");
+            Console.ReadKey();
+        }
+    }
+}
